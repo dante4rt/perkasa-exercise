6b3d55a934798c3bbc6d97e840dd12ca19f5c82e
dante4rt <rxmxdhxni@gmail.com>
2023-07-10 20:26:29 +0700

    second commit, setup react"

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { RouterProvider } from 'react-router-dom'
+import './App.css'
+import { Provider } from 'react-redux'
+
+function App() {
+  return (
+    <RouterProvider>
+
+    </RouterProvider>
+  )
+}
+
+export default App
